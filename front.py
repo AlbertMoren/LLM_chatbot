89664621cac9c1483c8efd94f8e8ea507b8c2537
AlbertMoren <albertmorenn@gmail.com>
2024-07-08 15:47:45 -0300

    version 0.8

--- front.py
+++ front.py
@@ -1,41 +1,33 @@
 import streamlit as st
 from streamlit_chat import message
-from utills.chatbot import create_conversation, create_vectorstore
-from utills.text import process_files,creat_text_chunks
+from utills  import chatbot
+from utills import text
 
 
 def main():
-    st.set_page_config(page_title="Pergunte para seus PDF",page_icon=':books:')
-
+    st.set_page_config(page_title="Pergunte para seus PDF", page_icon=':books:')
     st.header("LLm em")
-    userquestion = st.text_input("Faça uma pergunta para me")
+    user_question = st.text_input("Faça uma pergunta para mim")
 
-    if('conversation' not in st.session_state):
+    if 'conversation' not in st.session_state:
         st.session_state.conversation = None
-    
-    if (userquestion):
-        response = st.session_state.conversation(userquestion)['chat_hystory'] 
 
-        for i,text_mensage in enumerate(response):
-            
-            if (i % 2 == 0):
-                message(text_mensage.content, is_user=True, key=str(i) + '_user')
+    if user_question:
+        response = st.session_state.conversation(user_question)['chat_history']
+        for i, text_message in enumerate(response):
+            if i % 2 == 0:
+                message(text_message.content, is_user=True, key=str(i) + '_user')
             else:
-                message(text_mensage.content, is_user=False, key=str(i) + '_bot')
+                message(text_message.content, is_user=False, key=str(i) + '_bot')
 
     with st.sidebar:
-        st.subheader('arquivos')
-        pdf_docs = st.file_uploader('carregar arquivos',accept_multiple_files=True)
-        
+        st.subheader('Arquivos')
+        pdf_docs = st.file_uploader('Carregar arquivos', accept_multiple_files=True)
         if st.button("Processar"):
-            all_files_text = process_files(pdf_docs)
-
-            chunks = creat_text_chunks(all_files_text)
-
-            vectorstore = create_vectorstore(chunks)
-
-            st.session_state.conversation = create_conversation(vectorstore)
-
+            all_files_text = text.process_files(pdf_docs)
+            chunks = text.create_text_chunks(all_files_text)
+            vectorstore = chatbot.create_vectorstore(chunks)
+            st.session_state.conversation = chatbot.create_conversation(vectorstore)
 
 if __name__ == '__main__':
     main()
